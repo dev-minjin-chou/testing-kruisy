40dc5cc65bd7ef8ed15198b05028c96a79ce3e43
Minjin chou <49186771+kuntakinte777@users.noreply.github.com>
2021-11-22 09:43:15 +1100

    Update About.jsx

--- krocs_latest/src/components/About.jsx
+++ krocs_latest/src/components/About.jsx
@@ -24,7 +24,7 @@ const About = () => {
           <h2 className="sec-heading" data-aos="zoom-out"> LEGENDARY KROCS </h2>
         </div>
         <p>
-          The Legendary Krocs are the 33 Krocs that look after the other Krocs. Each owner of these
+          The Legendary Krocs are the 36 Krocs that look after the other Krocs. Each owner of these
           Legendary Kroc NFTs will stand the chance to walk away with a $20,000 bonus. Once all
           Kruisy Krocs are sold out, the Stonk Kroc will walk away with a bonus of $30,000.
         </p>
